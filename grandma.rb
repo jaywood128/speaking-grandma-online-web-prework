1b80eddd4dd8e24d40a6d1c516276ee7ed553945
Johnathon Wood <jaywood128@gmail.com>
2018-10-15 17:52:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    print"I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN"
   elsif phrase == phrase.downcase
-     puts"HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
  end
 end
   # Write a speak_to_grandma method.
