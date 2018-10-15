6558a82ea2aa66630e44f0c064a4a6511d861973
Johnathon Wood <jaywood128@gmail.com>
2018-10-15 17:55:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase != phrase.uppercase
      "HUH?! SPEAK UP, SONNY!"
  end
 end
